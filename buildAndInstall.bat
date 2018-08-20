b11afc6b27763c1ba7e36da24935ae2444ac81cb
rwg0 <robin.glover@workshare.com>
2018-08-20 21:43:30 +0100

    Build in release - fix builds for new setup name

--- buildAndInstall.bat
+++ buildAndInstall.bat
@@ -1,6 +1,6 @@
-"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" "./ASCOM.DSLR.sln" /p:configuration=debug /property:Platform="x86"
+"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" "./ASCOM.DSLR.sln" /p:configuration=release /property:Platform="x86"
 
-"c:\Program Files (x86)\Inno Setup 5\ISCC.exe" "./DSLR.Camera Setup.iss"
+"c:\Program Files (x86)\Inno Setup 5\ISCC.exe" "./DSLR.Camera.Setup.iss"
 
 ".\DSLR.Camera Setup.exe" /VERYSILENT /SUPPRESSMSGBOXES
 pause
